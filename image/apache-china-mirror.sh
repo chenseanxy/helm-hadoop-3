46a820c17dccded7eea513e741b58a6a39bbea1a
Chen Xinyang <chenxy1999728@126.com>
2019-10-25 13:24:09 +0800

    Added apache mirror for users in china

---
+++ image/apache-china-mirror.sh
@@ -0,0 +1,5 @@
+HADOOP_30_VERSION=3.2.1
+
+curl -LO https://mirrors.huaweicloud.com/apache/hadoop/common/hadoop-$HADOOP_30_VERSION/hadoop-$HADOOP_30_VERSION-src.tar.gz
+
+curl -LO https://mirrors.huaweicloud.com/apache/hadoop/common/hadoop-$HADOOP_30_VERSION/hadoop-$HADOOP_30_VERSION.tar.gz
